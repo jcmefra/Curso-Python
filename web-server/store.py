17b50e34777eb1dda3534b084023f5d9396d1c3f
Camilo Mejía <camilomejia55@gmail.com>
2023-07-08 15:27:45 -0500

    editado store.py

--- web-server/store.py
+++ web-server/store.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_categories():
-    r = requests.get('https://api.escuelajs.co/api/v1/products')
+    r = requests.get('https://api.escuelajs.co/api/v1/products') #Se hace una solicitud HTTPS con Requests
     print(r.status_code)
     print(r.text)
     print(type(r.text))
