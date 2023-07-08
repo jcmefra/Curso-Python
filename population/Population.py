a45b9fdb05ea7abc3fa4fc50672f8df3829733f6
Camilo Mejía <camilomejia55@gmail.com>
2023-07-08 17:46:36 -0500

    edit population.py

--- population/Population.py
+++ population/Population.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df1 = pd.read_csv('./population/data.csv') #Leer el CSV con Pandas
+df1 = pd.read_csv('data.csv') #Leer el CSV con Pandas
 df2 = df1.set_index('Country/Territory', drop = False) #Definir columna índice o principal
 continente = input('Type continent name: ')
 
