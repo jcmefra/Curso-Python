cd6294aa5c67c5398026b5607e3e722c3d470e8d
Camilo Mejía <camilomejia55@gmail.com>
2023-07-08 15:26:13 -0500

    main.py añadido

--- web-server/store.py
+++ web-server/store.py
@@ -3,3 +3,8 @@ import requests
 def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/products')
     print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json()
+    for category in categories:
+        print(category['title'])
