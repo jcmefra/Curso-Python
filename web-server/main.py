a8bf8067221f4741ed60fccf00e0409880f26bbb
Camilo Mejía <camilomejia55@gmail.com>
2023-07-10 19:29:08 -0500

    servidor creado y requirements.txt

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,24 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get ('/contact', response_class = HTMLResponse) #Puedo poner en el Return toda una página
+def get_list():
+    return """
+        <h1>Hola, soy Camilo </h1>
+        <p>El creador de este servidor</p>
+    """
 
 def run():
     store.get_categories()
 
 if __name__ == '__main__':
     run()
+
+#uvicorn main:app --reload -----> ejecutar en la terminal
